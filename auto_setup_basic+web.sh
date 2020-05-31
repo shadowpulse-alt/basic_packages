401c794ff94cb0e4105b62cc3cc664a67c9dd40b
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2020-05-31 16:23:04 +0200

    Add files via upload

---
+++ auto_setup_basic+web.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+apt update
+apt upgrade -y
+apt install -y sudo
+apt install -y qemu-guest-agent
+apt install -y speedtest-cli
+apt install -y net-tools
+apt install -y curl
+apt install -y git
+apt install -y apache2
+apt install -y php
