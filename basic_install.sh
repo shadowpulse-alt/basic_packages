cebed3bd13370d934f63d69a00d5accf09f12d08
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2020-06-29 10:29:47 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -3,4 +3,4 @@
 
 apt update
 apt upgrade -y
-apt install -y dos2unix sudo qemu-guest-agent speedtest-cli net-tools curl git wget
+apt install -y dos2unix sudo qemu-guest-agent speedtest-cli net-tools curl wget
