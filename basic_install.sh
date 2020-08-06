478a5db29214032d7c4b94a77fa681c476e06fae
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2020-08-07 01:41:02 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt update
-apt update && apt install -y dos2unix sudo speedtest-cli net-tools curl wget tasksel git
+apt update && apt install -y dos2unix sudo speedtest-cli net-tools curl wget tasksel git neofetch
