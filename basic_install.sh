2d5bfb627302e4018e6e28b87e5c8f17506c2aaf
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2020-08-13 02:06:18 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 
-apt update && apt install -y open-vm-tools dos2unix sudo speedtest-cli net-tools curl wget tasksel git neofetch
+apt update 
+apt install -y open-vm-tools         
+apt install -y dos2unix
+apt install -y sudo
+apt install -y speedtest-cli
+apt install -y net-tools
+apt install -y curl
+apt install -y wget
+apt install -y tasksel
+apt install -y git
+apt install -y neofetch
