4b23de5e16a987f9159380034097ad4e4179f797
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2020-09-28 15:13:16 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt update && apt full-upgrade -y
-apt install -y open-vm-tools qemu-guest-agent dos2unix sudo speedtest-cli net-tools curl wget tasksel git neofetch
+apt install -y open-vm-tools qemu-guest-agent dos2unix sudo speedtest-cli net-tools wget tasksel git neofetch
