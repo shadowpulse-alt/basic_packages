2fe4ed3074a4dab15c176798d4dfa963da72847a
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2020-09-28 15:18:52 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+
+#[1] Installation VM
 apt update && apt full-upgrade -y
-apt install -y open-vm-tools qemu-guest-agent dos2unix sudo speedtest-cli net-tools wget tasksel git neofetch
+apt install -y open-vm-tools qemu-guest-agent dos2unix sudo speedtest-cli net-tools tasksel git neofetch
+echo "Install complete"
+
+#[2] Installation classique
+apt update && apt full-upgrade -y
+apt install -y dos2unix sudo speedtest-cli net-tools tasksel git neofetch
+echo "Install complete"
