6cab9f46b6f1e2eb7f32c619319692f48b0e091e
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2020-11-02 21:31:06 +0100

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -3,6 +3,6 @@
 
 echo "Start Install"
 apt update && apt full-upgrade -y
-apt install -y dos2unix sudo speedtest-cli net-tools tasksel git neofetch
+apt install -y dos2unix sudo speedtest-cli net-tools tasksel git neofetch htop
 #open-vm-tools qemu-guest-agent 
 echo "Install Complete"
