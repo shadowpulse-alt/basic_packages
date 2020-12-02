84f13829643dd61ca11542d7d01309e352362d3b
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2020-12-02 01:04:22 +0100

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -3,6 +3,5 @@
 
 echo -e "Default \e[92mStart Install"
 apt update && apt full-upgrade -y
-apt install -y dos2unix sudo speedtest-cli net-tools tasksel git neofetch htop
-open-vm-tools qemu-guest-agent 
+apt install -y dos2unix sudo speedtest-cli net-tools tasksel git neofetch htop xrdp open-vm-tools qemu-guest-agent 
 echo -e "Default \e[92mInstall Complete"
