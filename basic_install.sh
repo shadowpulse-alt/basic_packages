02f404ba7ac17f6984df4ca0b7e5c8fbede36bbc
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2021-03-14 00:46:24 +0100

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 apt update && apt full-upgrade -y
-apt install -y dos2unix sudo speedtest-cli net-tools tasksel git neofetch htop xrdp screen 
-#qemu-guest-agent open-vm-tools
+apt install -y dos2unix sudo speedtest-cli net-tools tasksel git neofetch htop xrdp screen qemu-guest-agent open-vm-tools
