cc5618ae12ea6be73136e7fc19d637f38ebcecbf
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2022-01-08 08:11:36 +0100

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt update && apt full-upgrade -y
-apt install -y dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen qemu-guest-agent open-vm-tools
+apt install -y curl dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen qemu-guest-agent open-vm-tools
 npm install --global fast-cli
