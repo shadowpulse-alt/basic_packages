78f2dbde31e2dcabcefd71018664f3fe64c80555
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2022-05-28 17:51:04 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-apt update
-
-echo "Press any key to continue"
-while [ true ] ; do
-read -t 3 -n 1
-if [ $? = 0 ] ; then
-exit ;
-else
-echo "waiting for the keypress"
-fi
-done
-
-apt full-upgrade -y
+apt update && apt full-upgrade -y
 
 echo "Press any key to continue"
 while [ true ] ; do
