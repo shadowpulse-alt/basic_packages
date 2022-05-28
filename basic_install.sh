ea352f5c2564a2bd3daa782cec6b349d74e522eb
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2022-05-28 17:43:51 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-apt update && apt full-upgrade -y && apt install -y curl lm-sensors dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen qemu-guest-agent && npm install --global fast-cli && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && cd /home/"$USER" && 
+apt update && apt full-upgrade -y 
+&& apt install -y curl lm-sensors dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen qemu-guest-agent 
+&& npm install --global fast-cli 
+&& sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+&& cd /home/"$USER" 
+&& wget https://raw.githubusercontent.com/ElectroFactory/basic_packages/master/.zshrc
