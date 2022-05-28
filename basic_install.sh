695d9cdc30006cefeef5477b4759965c2aa218e7
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2022-05-28 04:09:46 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt update && apt full-upgrade -y && apt install -y curl dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen qemu-guest-agent && npm install --global fast-cli
+apt update && apt full-upgrade -y && apt install -y curl lm-sensors dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen qemu-guest-agent && npm install --global fast-cli
