141e9af2c24268d5ea3a6b80b005529199cb9143
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2022-05-28 22:29:13 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -17,12 +17,12 @@ sudo apt install -y zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/oh
 
 menu_option_four() {
   echo "Delete olds .zshrc files"
-cd /home/"$USER" && sudo rm .zshrc* -f
+cd /home/"$USER" && sudo rm .zshrc* -f && ls -la  | grep '.zshrc*'
 }
 
 menu_option_five() {
   echo "Pull new .zshrc file"
-cd /home/"$USER" && wget https://raw.githubusercontent.com/ElectroFactory/basic_packages/master/.zshrc
+cd /home/"$USER" && wget https://raw.githubusercontent.com/ElectroFactory/basic_packages/master/.zshrc && ls -la  | grep '.zshrc*'
 }
 
 menu_option_six() {
