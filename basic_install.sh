bfdca8d3a6e188cdda653ecb59ed4c88e64db8b1
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2022-07-28 04:12:24 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -10,7 +10,7 @@ apt full-upgrade -y
 
 menu_option_2() {
   echo "Install Packages"
-sudo apt install -y curl lm-sensors dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen iperf3 qemu-guest-agent realmd sssd-tools sssd libnss-sss libpam-sss adcli samba-common
+sudo apt install -y curl lm-sensors dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen iperf3 qemu-guest-agent realmd sssd-tools sssd libnss-sss libpam-sss adcli samba-common apt-get install zstd
 }
 
 menu_option_3() {
