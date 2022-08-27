d32e690f13ac08eec6d40bc5637f28b5948da8a4
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2022-08-27 21:32:56 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -68,9 +68,6 @@ menu_option_11() {
   echo "Autorize ssh login as root"
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
 cat /etc/ssh/sshd_config
-echo -n "      Press Enter to continue "
-read
-
 }
 
 press_enter() {
