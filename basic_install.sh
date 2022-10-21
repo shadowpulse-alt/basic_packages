f4c925b1d0785f48d032081329a2e58357adf89b
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2022-10-22 01:33:09 +0200

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -88,8 +88,9 @@ echo "Pull zabbix-agent configuration file"
         rm /etc/zabbix/zabbix_agentd.conf
         fi
         echo "Pull file"
-        cd /etc/zabbix/
+        cd /root
         wget https://raw.githubusercontent.com/ElectroFactory/basic_packages/master/zabbix_agentd.conf
+        mv /root/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
         echo "Configure hostname into configuration file"
 	      sudo sed -i "s/Hostname=/Hostname=$HOSTNAME/g" /etc/zabbix/zabbix_agentd.conf
 	      sudo sed -n 146p /etc/zabbix/zabbix_agentd.conf
