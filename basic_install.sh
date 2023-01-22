3345b9ed7f429556cdfeb68eafa1f5bed989d9d6
ElectroFactory <48736803+ElectroFactory@users.noreply.github.com>
2023-01-22 20:05:40 +0100

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-echo "Basic packages installer V3.7.1"
+noir='\e[0;30m'
+gris='\e[1;30m'
+rougefonce='\e[0;31m'
+rose='\e[1;31m'
+vertfonce='\e[0;32m'
+vertclair='\e[1;32m'
+orange='\e[0;33m'
+jaune='\e[1;33m'
+bleufonce='\e[0;34m'
+bleuclair='\e[1;34m'
+violetfonce='\e[0;35m'
+violetclair='\e[1;35m'
+cyanfonce='\e[0;36m'
+cyanclair='\e[1;36m'
+grisclair='\e[0;37m'
+blanc='\e[1;37m'
+neutre='\e[0;m'
+
+
+echo -e "${bleuclair}Basic packages installer V3.8.0${neutre}"
 
 menu_option_01() {
 echo "Update"
