399e99f6083c0be30d6f5240683d03122993f386
edroz <48736803+EdR0Z@users.noreply.github.com>
2023-11-22 05:02:37 +0100

    Update basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -20,11 +20,13 @@ echo "Update"
 
 menu_option_02() {
 echo "Install Packages"
+	apt install -y sudo
         sudo apt install -y zsh-common zip unzip zsh-doc cifs-utils wget curl lm-sensors sshpass dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen iperf3 samba-common zstd apt-transport-https ca-certificates gnupg2 software-properties-common
 }
 
 menu_option_03() {
 echo "Install Oh-My-Zsh"
+	apt install -y sudo
         sudo apt install -y git curl
         sudo apt install -y zsh
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && exit
@@ -52,6 +54,7 @@ echo "Find .zshrc file at home directory (classic user)"
 
 menu_option_07() {
 echo "Delete olds .zshrc files (root user)"
+	apt install -y sudo
         cd /root 
         sudo rm .zshrc* -f
         ls -la
@@ -77,6 +80,7 @@ echo "Check current folder"
 
 menu_option_11() {
 echo "Autorize ssh login as root"
+	apt install -y sudo
         sudo sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
         sudo sed -n 32p /etc/ssh/sshd_config
         sudo systemctl restart sshd
@@ -84,6 +88,7 @@ echo "Autorize ssh login as root"
 
 menu_option_12() {
 echo "Install nala-legacy"
+	apt install -y sudo
         sudo wget -qO- https://deb.volian.org/volian/scar.key | gpg --dearmor | dd of=/usr/share/keyrings/volian-archive-scar.gpg && sudo echo "deb [signed-by=/usr/share/keyrings/volian-archive-scar.gpg arch=amd64] https://deb.volian.org/volian/ scar main" > /etc/apt/sources.list.d/volian-archive-scar.list && sudo apt update && sudo apt install -y nala-legacy
         export LC_ALL=C.UTF-8
         export LANG=C.UTF-8
@@ -92,6 +97,7 @@ echo "Install nala-legacy"
 
 menu_option_13() {
 echo "Install zabbix-agent for linux"
+	apt install -y sudo
         apt install -y zabbix-agent curl wget sudo
         sudo systemctl enable zabbix-agent
         sudo systemctl start zabbix-agent
@@ -151,6 +157,7 @@ echo "Show hostname and IPv4"
 
 menu_option_15() {
 echo "Uninstall zabix-agent for linux"
+	apt install -y sudo
         sudo systemctl stop zabbix-agent
         sudo apt remove -y zabbix-agent
         sudo apt autopurge -y
@@ -159,21 +166,24 @@ echo "Uninstall zabix-agent for linux"
 
 menu_option_16() {
 echo "Activate num lock at startup for debian"
-echo -e "#!/bin/bash \nfor tty in /dev/tty[1-6]; do \n/usr/bin/setleds -D +num < $tty \ndone" >> /etc/rc.local
+	echo -e "#!/bin/bash \nfor tty in /dev/tty[1-6]; do \n/usr/bin/setleds -D +num < $tty \ndone" >> /etc/rc.local
 }
 
 menu_option_17() {
 echo "Install guests addons for proxmox"
-sudo apt install -y qemu-guest-agent
+	apt install -y sudo
+	sudo apt install -y qemu-guest-agent
 }
 
 menu_option_18() {
 echo "Install guests addons for ESXi"
-sudo apt install -y open-vm-tools
+	apt install -y sudo
+	sudo apt install -y open-vm-tools
 }
 
 menu_option_19() {
 echo "Autorize gnome login as root"
+	apt install -y sudo
         sudo sed -i "s/auth required pam_succeed_if.so user != root quiet_success/#auth required pam_succeed_if.so user != root quiet_success/g" /etc/pam.d/gdm-password
         sudo sed -n 3p /etc/pam.d/gdm-password
         sudo sed -i "s/auth required pam_succeed_if.so user != root quiet/#auth required pam_succeed_if.so user != root quiet/g" /etc/pam.d/gdm-password
@@ -182,18 +192,21 @@ echo "Autorize gnome login as root"
 
 menu_option_20() {
 echo "Deactivate CD-ROM sources from sources.list configuration file"
+	apt install -y sudo
         sudo sed -i "s/deb cdrom:/#deb cdrom:/g" /etc/apt/sources.list
         sudo sed -n 3p /etc/apt/sources.list
 }
 
 menu_option_21() {
 echo "fix the fans at the 60 level of the pwm"
+	apt install -y sudo sshpass
 	sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p global unlock"
         sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p global lock 60"
 }
 
 menu_option_22() {
 echo "let the fans vary between 60 and 120 pwm levels"
+	apt install -y sudo sshpass
 	sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p global unlock" 
         sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p 0 max 120"
 	sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p 1 max 120"
@@ -215,6 +228,7 @@ echo "let the fans vary between 60 and 120 pwm levels"
 
 menu_option_23() {
 echo "let the fans vary between 60 and 150 pwm levels"
+	apt install -y sudo sshpass
         sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p global unlock"        
         sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p 0 max 150"
 	sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p 1 max 150"
@@ -236,6 +250,7 @@ echo "let the fans vary between 60 and 150 pwm levels"
 
 menu_option_24() {
 echo "let the fans vary between 60 and 255 pwm levels"
+	apt install -y sudo sshpass
         sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p global unlock"		
         sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p 0 max 255"
 	sshpass -p 'Clement27930' ssh admin@192.168.1.79 -o KexAlgorithms=+diffie-hellman-group14-sha1 -o HostKeyAlgorithms=ssh-rsa -o HostKeyAlgorithms=ssh-dss -o HostKeyAlgorithms=ssh-rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "fan p 1 max 255"
@@ -257,6 +272,7 @@ echo "let the fans vary between 60 and 255 pwm levels"
 
 menu_option_25() {
 echo "automount plex medias folder libraries"
+	apt install -y sudo sshpass
         sshpass -p '910805' ssh root@192.168.1.65 "mount -a"
 }
 
