b643ede60cd6735ccb01b5c2f6078df14a577b1d
ShadowPulse <clement.dugeay@gmail.com>
2025-06-29 09:04:23 +0200

    Refactor menu options and update descriptions in basic_install.sh

--- basic_install.sh
+++ basic_install.sh
@@ -19,69 +19,66 @@ echo "Update"
 }
 
 menu_option_02() {
-echo "Install Packages"
+echo "Install Base Packages"
         sudo apt install -y zsh-common zip unzip zsh-doc cifs-utils wget curl lm-sensors sshpass dos2unix sudo net-tools tasksel git npm neofetch htop xrdp screen iperf3 samba-common zstd apt-transport-https ca-certificates gnupg2 software-properties-common
 }
 
 menu_option_03() {
+echo "Install Oh-My-ZSH + Pull user config file (Non privilegied user)"
+
 echo "Install Oh-My-Zsh"
         sudo apt install -y git curl
         sudo apt install -y zsh
         sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && exit
-}
 
-menu_option_04() {
-echo "Delete olds .zshrc files (classic user)"
         cd /home/"$USER" 
         sudo rm .zshrc* -f
         ls -la
-}
 
-menu_option_05() {
-echo "Pull new .zshrc file (classic user)"
+        echo "Pull new .zshrc file (classic user)"
         cd /home/"$USER"
         sudo wget https://raw.githubusercontent.com/ElectroFactory/basic_packages/master/.zshrc
         ls -la
-}
 
-menu_option_06() {
-echo "Find .zshrc file at home directory (classic user)"
+        echo "Find .zshrc file at home directory (Non privilegied user"
         cd /home/"$USER" 
         ls -la
 }
 
-menu_option_07() {
-echo "Delete olds .zshrc files (root user)"
+menu_option_04() {
+echo "Install Oh-My-ZSH + Pull user config file (root user)"
+
+echo "Install Oh-My-Zsh"
+        sudo apt install -y git curl
+        sudo apt install -y zsh
+        sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && exit
+
         cd /root 
         sudo rm .zshrc* -f
         ls -la
-}
 
-menu_option_08() {
 echo "Pull new .zshrc file (root user)"
         cd /root
         wget https://raw.githubusercontent.com/ElectroFactory/basic_packages/master/.zshrc
         ls -la
-}
 
-menu_option_09() {
 echo "Find .zshrc file at home directory (root user)"
         cd /root
         ls -la
 }
 
-menu_option_10() {
+menu_option_05() {
 echo "Check current folder"
         pwd
 }
 
-menu_option_11() {
+menu_option_06() {
 echo "Autorize ssh login as root"
         sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
         sudo systemctl restart sshd
 }
 
-menu_option_12() {
+menu_option_07() {
 echo "Install nala-legacy"
         sudo wget -qO- https://deb.volian.org/volian/scar.key | gpg --dearmor | dd of=/usr/share/keyrings/volian-archive-scar.gpg && sudo echo "deb [signed-by=/usr/share/keyrings/volian-archive-scar.gpg arch=amd64] https://deb.volian.org/volian/ scar main" > /etc/apt/sources.list.d/volian-archive-scar.list && sudo apt update && sudo apt install -y nala-legacy
         export LC_ALL=C.UTF-8
@@ -89,7 +86,7 @@ echo "Install nala-legacy"
         sudo nala update
 }
 
-menu_option_13() {
+menu_option_08() {
 echo "Install zabbix-agent for linux"
         apt install -y zabbix-agent curl wget sudo
         sudo systemctl enable zabbix-agent
@@ -141,14 +138,14 @@ echo "Install zabbix-agent for linux"
         echo ">>>>>> <<<<<< >>>>>> <<<<<< >>>>>> <<<<<< >>>>>> <<<<<< >>>>>> <<<<<< >>>>>> <<<<<< >>>>>> <<<<<< >>>>>> <<<<<< >>>>>> <<<<<<"
 }
 
-menu_option_14() {
+menu_option_09() {
 echo "Show hostname and IPv4"
         cat /proc/sys/kernel/hostname
         hostname -I
         
 }
 
-menu_option_15() {
+menu_option_10() {
 echo "Uninstall zabix-agent for linux"
         sudo systemctl stop zabbix-agent
         sudo apt remove -y zabbix-agent
@@ -156,22 +153,22 @@ echo "Uninstall zabix-agent for linux"
         sudo rm -rf /etc/zabbix
 }
 
-menu_option_16() {
+menu_option_11() {
 echo "Activate num lock at startup for debian"
 echo -e "#!/bin/bash \nfor tty in /dev/tty[1-6]; do \n/usr/bin/setleds -D +num < $tty \ndone" >> /etc/rc.local
 }
 
-menu_option_17() {
+menu_option_12() {
 echo "Install guests addons for proxmox"
 sudo apt install -y qemu-guest-agent
 }
 
-menu_option_18() {
+menu_option_13() {
 echo "Install guests addons for ESXi"
 sudo apt install -y open-vm-tools
 }
 
-menu_option_19() {
+menu_option_14() {
 echo "Autorize gnome login as root"
         sudo sed -i "s/auth required pam_succeed_if.so user != root quiet_success/#auth required pam_succeed_if.so user != root quiet_success/g" /etc/pam.d/gdm-password
         sudo sed -n 3p /etc/pam.d/gdm-password
@@ -179,7 +176,7 @@ echo "Autorize gnome login as root"
         sudo sed -n 3p /etc/pam.d/gdm-password
 }
 
-menu_option_20() {
+menu_option_15() {
 echo "Deactivate CD-ROM sources from sources.list configuration file"
         sudo sed -i "s/deb cdrom:/#deb cdrom:/g" /etc/apt/sources.list
         sudo sed -n 3p /etc/apt/sources.list
@@ -200,28 +197,22 @@ until [ "$selection" = "0" ]; do
   clear
   echo -e ""
   echo -e "        ${brightwhite}[01]  -  Update"
-  echo -e "        ${brightgreen}[02]  -  Install Packages"
-  echo -e "        ${magenta}[03]  -  Install Oh-My-Zsh"
-  echo -e "        ${brightcyan}[04]  -  Delete olds .zshrc files (classic user)"
-  echo -e "        ${brightcyan}[05]  -  Pull new .zshrc file (classic user)"
-  echo -e "        ${brightcyan}[06]  -  Find .zshrc file at home directory (classic user)"
+  echo -e "        ${brightgreen}[02]  -  Install Base Packages"
+  echo -e "        ${magenta}[03]  -  Install Oh-My-ZSH + Pull user config file (Non privilegied user)"
+  echo -e "        ${brightcyan}[04]  -  Install Oh-My-ZSH + Pull user config file (root user)"
+  echo -e "        ${brightcyan}[05]  -  Check current folder"
+  echo -e "        ${brightcyan}[06]  -  Autorize ssh login as root"
   
-  echo -e "        ${red}[07]  -  Delete olds .zshrc files (root user)"
-  echo -e "        ${red}[08]  -  Pull new .zshrc file (root user)"
-  echo -e "        ${red}[09]  -  Find .zshrc file at home directory (root user)"
-  echo -e "        ${brightwhite}[10]  -  Check current folder"
-  echo -e "        ${red}[11]  -  Autorize ssh login as root"
-  echo -e "        ${brightcyan}[12]  -  Install nala-legacy"
+  echo -e "        ${red}[07]  -  Install nala-legacy"
+  echo -e "        ${red}[08]  -  Install zabbix-agent for linux"
+  echo -e "        ${red}[09]  -  Show hostname and IPv4"
+  echo -e "        ${brightwhite}[10]  -  Uninstall zabix-agent for linux"
+  echo -e "        ${red}[11]  -  Activate num lock at startup for debian"
+  echo -e "        ${brightcyan}[12]  -  Install guests addons for proxmox"
   
-  echo -e "        ${cyan}[13]  -  Install zabbix-agent for linux"  
-  echo -e "        ${cyan}[14]  -  Show hostname and IPv4"
-  echo -e "        ${red}[15]  -  Uninstall zabix-agent for linux"
-  echo -e "        ${brightwhite}[16]  -  Activate num lock at startup for debian"
-  echo -e "        ${blue}[17]  -  Install guests addons for proxmox"
-  echo -e "        ${blue}[18]  -  Install guests addons for ESXi"
-  
-  echo -e "        ${red}[19]  -  Autorize gnome login as root"
-  echo -e "        ${brightwhite}[20]  -  Deactivate CD-ROM sources from sources.list configuration file"
+  echo -e "        ${cyan}[13]  -  Install guests addons for ESXi"  
+  echo -e "        ${cyan}[14]  -  Autorize gnome login as root"
+  echo -e "        ${red}[15]  -  Deactivate CD-ROM sources from sources.list configuration file"
   
   echo -e "        ${brightwhite}[00]  -  Exit"
   echo -e "        ${brightwhite}"
@@ -245,11 +236,6 @@ until [ "$selection" = "0" ]; do
     13 )  clear ; menu_option_13 ; press_enter ;;
     14 )  clear ; menu_option_14 ; press_enter ;;
     15 )  clear ; menu_option_15 ; press_enter ;;
-    16 )  clear ; menu_option_16 ; press_enter ;;
-    17 )  clear ; menu_option_17 ; press_enter ;;
-    18 )  clear ; menu_option_18 ; press_enter ;;
-    19 )  clear ; menu_option_19 ; press_enter ;;
-    20 )  clear ; menu_option_20 ; press_enter ;;
     00 )  clear ; exit ;;
     *  )  clear ; incorrect_selection ; press_enter ;;
     esac
